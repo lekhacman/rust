9fd384020a5671dcd96fc80760e51f5163060d42
Khac Man <khacmanle@gmail.com>
2019-01-30 00:44:13 +0800

    loop syntax

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+
+    let a = [10, 20, 30, 40, 50];
+
+    for e in a.iter() {
+        println!("Value: {}", e);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
 }
