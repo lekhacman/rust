c7deec49a2b36c76fffbb8154a4dca1a9f352972
Andrew Le <andrew_le@aviva-asia.com>
2019-02-01 00:15:43 +0800

    referencing & borrowing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,4 @@
+
 fn main() {
     let s1 = gives_ownership();
 
@@ -17,6 +18,15 @@ fn main() {
     let text = String::from("Hello World!");
     let (s4, len) = calculate_length(text);
     println!("length of '{}' is {}", s4, len);
+
+    let mut another_string = String::from("Hello Man");
+    // get_length does not own another_string
+    let another_string_len = get_length(&another_string);
+    println!("length of '{}' is {}", another_string, another_string_len);
+
+    append_sth(&mut another_string);
+    println!("length of '{}' is {}", another_string, another_string_len);
+
 }
 
 fn takes_ownership(str: String) {
@@ -36,7 +46,15 @@ fn takes_n_gives_back(str: String) -> String {
     str
 }
 
+fn get_length(str: &String) -> usize {
+    str.len()
+}
+
 fn calculate_length(s: String) -> (String, usize) {
     let length = s.len();
     (s, length)
+}
+
+fn append_sth(s: &mut String) {
+    s.push_str(" Le");
 }
