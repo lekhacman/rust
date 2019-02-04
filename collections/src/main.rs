7ae83f1bc80fe6ee03806d349deb674f6f3b206e
Andrew Le <andrew_le@aviva-asia.com>
2019-02-05 02:10:38 +0800

    Vector

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,5 +1,30 @@
 fn main() {
-    let other_v: Vec<i32> = Vec::new();
-    let v = vec![1, 2, 3];
+    let mut other_v: Vec<i32> = Vec::new();
+    let v = vec![1, 2, 3, 4];
 
+    other_v.push(5);
+    other_v.push(6);
+    other_v.push(7);
+    other_v.push(8);
+
+    let third = &v[2];
+    println!("Third: {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("third {}", third),
+        None => println!("No third element"),
+    };
+
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut vec = vec![100, 32, 57];
+    for i in &mut vec {
+        *i += 50;
+    }
+
+    for i in vec {
+        println!("{}", i);
+    }
 }
