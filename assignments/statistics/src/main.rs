582ad6677bf1f846f1cc99c6c2efba095676485b
Andrew Le <andrew_le@aviva-asia.com>
2019-02-08 02:44:02 +0800

    Statistics computer assignment

---
+++ assignments/statistics/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/**
+Given a list of integers, use a vector
+and return the mean (the average value),
+median (when sorted,
+the value in the middle position),
+and mode (the value that occurs most often;
+a hash map will be helpful here) of the list.
+*/
+mod stats {}
