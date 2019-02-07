028c4fb90d9dfdba95055a796eabc75678784bed
Andrew Le <andrew_le@aviva-asia.com>
2019-02-08 03:23:58 +0800

    [Stats] mean

--- assignments/statistics/src/main.rs
+++ assignments/statistics/src/main.rs
@@ -1,13 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let collection = vec![1, 6, 5 , 1];
+
+    assert_eq!(stats::mean(&collection), 3.25);
+    assert_eq!(stats::mean(&vec![2,4]), 3.0);
+
 }
 
 /**
 Given a list of integers, use a vector
 and return the mean (the average value),
-median (when sorted,
-the value in the middle position),
+median (when sorted, the value in the middle position),
 and mode (the value that occurs most often;
 a hash map will be helpful here) of the list.
 */
-mod stats {}
+mod stats {
+    pub fn mean(collection: &Vec<i32>) -> f64 {
+        let mut sum = 0.0;
+        for val in collection {
+            sum += f64::from(*val);
+        }
+
+        sum / (collection.len() as f64)
+    }
+}
