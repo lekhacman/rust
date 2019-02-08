4c619879e1e44f7d0a1ebbf65443b961a18a8a84
Andrew Le <andrew_le@aviva-asia.com>
2019-02-08 11:25:19 +0800

    [Stats] mode, median

--- assignments/statistics/src/main.rs
+++ assignments/statistics/src/main.rs
@@ -4,6 +4,20 @@ fn main() {
     assert_eq!(stats::mean(&collection), 3.25);
     assert_eq!(stats::mean(&vec![2,4]), 3.0);
 
+    assert_eq!(stats::mode(&collection), vec![1]);
+
+    let collection2 = vec![2,3,2,5,7,8,7];
+    assert_eq!(stats::mode(&collection), vec![1]);
+
+    let mut modes2 = stats::mode(&collection2);
+    modes2.sort();
+    assert_eq!(modes2, vec![2, 7]);
+
+    assert_eq!(stats::median(&vec![1,2,3]), 2.0);
+    assert_eq!(stats::median(&vec![1]), 1.0);
+    assert_eq!(stats::median(&vec![2, 4, 3, 5]), 3.5);
+    assert_eq!(stats::median(&collection), 3.0);
+    assert_eq!(stats::median(&collection2), 5.0);
 }
 
 /**
@@ -14,12 +28,49 @@ and mode (the value that occurs most often;
 a hash map will be helpful here) of the list.
 */
 mod stats {
-    pub fn mean(collection: &Vec<i32>) -> f64 {
-        let mut sum = 0.0;
-        for val in collection {
-            sum += f64::from(*val);
-        }
+    use std::collections::HashMap;
+    use std::prelude::v1::Vec;
 
-        sum / (collection.len() as f64)
+    pub fn mean(collection: &Vec<i32>) -> f64 {
+        let sum: i32 = collection.iter().sum();
+        f64::from(sum) / (collection.len() as f64)
+    }
+
+    pub fn mode(collection: &Vec<i32>) -> Vec<i32> {
+        let mut map: HashMap<i32, i32> = HashMap::new();
+        for val in collection {
+            let count = map.entry(*val).or_insert(0);
+            *count += 1;
+        }
+        let mut result = Vec::new();
+        let mut roof = 0;
+        for (key, value) in map.iter() {
+            if *value > roof {
+                roof = *value;
+                result.clear();
+                result.push(*key);
+            } else if *value == roof {
+                result.push(*key);
+            }
+        }
+        result
+    }
+
+    pub fn median(collection: &Vec<i32>) -> f64 {
+        let mut sorted = collection.clone();
+        sorted.sort();
+        let len = sorted.len() as i32;
+
+        let result = if len % 2 != 0 {
+            let n = ((len + 1) / 2) as usize;
+            let value = &sorted[(n - 1)];
+            f64::from(*value)
+
+        } else {
+            let n = (len / 2) as usize;
+            mean(&vec![sorted[n-1], sorted[n]])
+        };
+
+        result
     }
 }
