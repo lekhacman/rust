b014c374c2bc33f857318e6402277e7691ef24e5
Khac Man <khacmanle@gmail.com>
2019-02-23 17:29:48 +0800

    fix largest fn

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -9,6 +9,17 @@ fn main() {
     notify(tweet)
 }
 
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 pub fn notify<T: Summary>(item: T) {
     println!("Breaking news! {}", item.summarize());
 }
