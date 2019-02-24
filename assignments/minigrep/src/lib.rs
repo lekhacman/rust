be43a8f6e89428b996b0a27cf6a4bbac3ef51314
Khac Man <khacmanle@gmail.com>
2019-02-25 00:00:16 +0800

    refactoring

---
+++ assignments/minigrep/src/lib.rs
@@ -0,0 +1,27 @@
+use std::fs;
+use std::error::Error;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With tetx: \n{}", contents);
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
--- assignments/minigrep/src/main.rs
+++ assignments/minigrep/src/main.rs
@@ -1,17 +1,22 @@
 use std::env;
-use std::fs;
+use std::process;
+use minigrep;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args)
+        .unwrap_or_else(|err| {
+            println!("Problem parsing arguments {}", err);
+            process::exit(1);
+        });
 
-    println!("Searching for {}",  query);
-    println!("In file {}", filename);
+    println!("Searching for {}",  config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
-        .expect("Failed to read file");
-
-    println!("With tetx: \n{}", contents);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
