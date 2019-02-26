efed5c85fa3ef3b4ea1acbb24ff800b35f01251b
Khac Man <khacmanle@gmail.com>
2019-02-27 00:46:25 +0800

    Test Config::new()

--- assignments/minigrep/src/lib.rs
+++ assignments/minigrep/src/lib.rs
@@ -25,3 +25,29 @@ impl Config {
         Ok(Config { query, filename })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::Config;
+
+    #[test]
+    fn new_config() {
+        let first_arg = String::from("first");
+        let second_arg = String::from("second");
+        let third_arg = String::from("third");
+        let args = [
+            first_arg.clone(),
+            second_arg.clone(),
+            third_arg.clone(),
+        ];
+        let config = Config::new(&args)
+            .unwrap_or_else(|_| {panic!("failed")});
+        assert_eq!(third_arg, config.filename);
+        assert_eq!(second_arg, config.query);
+
+        let failed_config = Config::new(&[
+            String::from("Hello world!"),
+        ]);
+        assert!(failed_config.is_err());
+    }
+}
