e2f752752df0180f5e8b23d2e32fc25dfadaa6a2
Khac Man <khacmanle@gmail.com>
2019-02-27 22:14:10 +0800

    Completed mini grep

--- assignments/minigrep/src/lib.rs
+++ assignments/minigrep/src/lib.rs
@@ -4,9 +4,24 @@ use std::error::Error;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With tetx: \n{}", contents);
+    let result = search(&config.query, &contents);
+
+    for line in result.iter() {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+fn search(query: &str, content: &str) -> Vec<String> {
+    let mut result = vec![];
+    for line in content.lines() {
+        if line.contains(query) {
+            result.push(line.to_string())
+        }
+    }
+
+    result
 }
 
 pub struct Config {
@@ -29,6 +44,8 @@ impl Config {
 #[cfg(test)]
 mod tests {
     use crate::Config;
+    use crate::run;
+    use crate::search;
 
     #[test]
     fn new_config() {
@@ -49,5 +66,32 @@ mod tests {
             String::from("Hello world!"),
         ]);
         assert!(failed_config.is_err());
+    }
+
+    #[test]
+    fn test_run() {
+        let config = Config {
+            query: String::from("long"),
+            filename: String::from("poem.txt"),
+        };
+        let result = run(config);
+        assert!(result.is_ok());
+
+        let fail_result = run(Config{
+            query: String::from("abc"),
+            filename: String::from("abc.txt"),
+        });
+        assert!(fail_result.is_err());
+    }
+
+    #[test]
+    fn test_search() {
+        let content = String::from("Hello \nWorld \n or friend");
+        let query = String::from("or");
+
+        let lines = search(&query, &content);
+        assert_eq!(lines.len(), 2);
+        assert_eq!(lines[0], "World ".to_string());
+        assert_eq!(lines[1], " or friend".to_string());
     }
 }
