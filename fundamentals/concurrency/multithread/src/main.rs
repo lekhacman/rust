5458737559ddb2cf6df827f3653aba5f9d6b0626
Khac Man <khacmanle@gmail.com>
2019-03-07 23:51:12 +0800

    Thread

--- fundamentals/concurrency/multithread/src/main.rs
+++ fundamentals/concurrency/multithread/src/main.rs
@@ -2,8 +2,16 @@ use std::time::Duration;
 use std::thread;
 
 fn main() {
-    println!("Num of thread: {}", thread::);
-    thread::spawn(||  {
+//    println!("Num of thread: {}", thread::);
+    let v = vec![1, 2, 3];
+
+    let other_handle = thread::spawn(move || {
+        println!("vec: {:?}", v);
+    });
+
+    other_handle.join();
+
+    let handle = thread::spawn(||  {
         for i in 1..10 {
             println!("from spawned thread {}", i);
             thread::sleep(Duration::from_millis(1))
@@ -14,4 +22,6 @@ fn main() {
         println!("from main thread {}", i);
         thread::sleep(Duration::from_millis(1))
     }
+
+    handle.join();
 }
